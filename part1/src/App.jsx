0252c1cd8237e59ede6ca112c457be1578693748
Sabina Zaman <sabina.zaman94@gmail.com>
2024-02-09 12:02:33 +0100

    1_2 implementation

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,12 +8,22 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-      {props.partName} {props.exerciseName}
-      </p>
+    <Part part = {props.part1} exercise = {props.exercises1}/>
+    <Part part = {props.part2} exercise= {props.exercises2}/>
+    <Part part = {props.part3} exercise = {props.exercises3}/>
     </div>
   )
 }
@@ -40,9 +50,11 @@ const App = () => {
   return (
     <div>
       <Header courseName={course}/> 
-      <Content partName={part1} exerciseName={exercises1}/>
-      <Content partName={part2} exerciseName={exercises2}/>
-      <Content partName={part3} exerciseName={exercises3}/>
+      <Content 
+        part1 = {part1} exercises1 = {exercises1}
+        part2={part2} exercises2 = {exercises1}
+        part3={part3} exercises3 = {exercises1}
+      />
       <Total totalCount = {exercises1 + exercises2 + exercises3} />
      
     </div>
