6f77bb59b317b391f12456629f34218ae133cd0e
Sabina Zaman <sabina.zaman94@gmail.com>
2024-02-10 07:26:10 +0100

    2.9*: The Phonebook Step 4

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,13 +1,18 @@
 
 import React, { useState } from 'react'
+import Person from "./component/persons"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "234-56789", id: 1
-   }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newPersons, setNewPersons] = useState('')
   const [newNumbers, setNewNumbers] = useState('')
+  const [newSearch, setSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -20,6 +25,7 @@ const App = () => {
       }
       setPersons(persons.concat(personObject))
       setNewPersons('')
+      setNewNumbers('')
     }
   }
 
@@ -31,9 +37,23 @@ const App = () => {
     setNewNumbers(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value) 
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLocaleLowerCase().includes(newSearch.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <p>filter shown with: <input value={newSearch} onChange={handleSearch} /></p>
+        <ul>
+        {filteredPersons.map(person => (
+          <Person key={person.id} person={person} />
+        ))}
+      </ul>
       <form onSubmit={addPerson}>
         <p>name: <input value={newPersons} onChange={handleNameChange} /></p>
         <p>number: <input value={newNumbers} onChange={handleNumberChange} /> </p>
@@ -51,3 +71,6 @@ export default App
 
 // form -> event method -> new object create -> concat with real object
 // input value -> handler event method -> will set value to newPersons
+// filteredPersons method is used to filter persons.name which includes newSearch
+//  if (persons.some(person => person.name === newPersons)) ---> kind of mapping all 
+//                                                    person name and looking for minimum one match
