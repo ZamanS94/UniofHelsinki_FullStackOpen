9d04b653a5b93d29a7da5f5d9ba0161e2c5c2490
Sabina Zaman <sabina.zaman94@gmail.com>
2024-02-10 01:23:52 +0100

    1.7 unicafe step2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,42 +1,49 @@
 import { useState } from 'react'
 
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
+    const updatedgood = good + 1
     setGood(good + 1)
+    setTotal(updatedgood+bad+neutral)
+    console.log("g",total)
   }
 
   const handleNeutralClick = () => {
+    const updatedneutral = neutral +1
     setNeutral(neutral + 1)
+    setTotal(good+bad+updatedneutral)
+    console.log("n",total)
   }
 
   const handleBadClick = () => {
+    const updatedbad = bad +1
     setBad(bad + 1)
+    setTotal(good+neutral+updatedbad)
+    console.log("bad",total)
   }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handleClick={handleGoodClick} text='good' />
-      <Button handleClick={handleNeutralClick} text='neutral' />
-      <Button handleClick={handleBadClick} text='bad' />
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad ) / total}</p>
+      <p>positive {good/total*100}%</p>
     </div>
   )
 }
 
 export default App
+
 
