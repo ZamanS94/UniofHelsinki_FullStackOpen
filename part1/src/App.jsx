72d15422d9e89cdb20e57a2dfb33210a3b7f9387
Sabina Zaman <sabina.zaman94@gmail.com>
2024-02-10 01:31:29 +0100

    1_8 unicafe step3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {(props.good - props.bad) / props.total}</p>
+      <p>positive {props.good / props.total}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -7,24 +21,21 @@ const App = () => {
   const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
-    const updatedgood = good + 1
-    setGood(good + 1)
-    setTotal(updatedgood+bad+neutral)
-    console.log("g",total)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + bad + neutral)
   }
 
   const handleNeutralClick = () => {
-    const updatedneutral = neutral +1
-    setNeutral(neutral + 1)
-    setTotal(good+bad+updatedneutral)
-    console.log("n",total)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + bad + updatedNeutral)
   }
 
   const handleBadClick = () => {
-    const updatedbad = bad +1
-    setBad(bad + 1)
-    setTotal(good+neutral+updatedbad)
-    console.log("bad",total)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
   }
 
   return (
@@ -33,17 +44,14 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>Bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good - bad ) / total}</p>
-      <p>positive {good/total*100}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral} total={total} />
     </div>
   )
 }
 
 export default App
+
+
+
 
 
