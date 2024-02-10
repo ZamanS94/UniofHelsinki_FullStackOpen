1e222729afd31366840b79004e125fdd5528e71e
Sabina Zaman <sabina.zaman94@gmail.com>
2024-02-10 02:43:58 +0100

    1_14* anecdotes step3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
-)
+);
 
 const Button2 = ({ handleRate, text }) => (
   <button onClick={() => handleRate()}>
     {text}
   </button>
-)
+);
+
+const MaxRate = ({value,show}) => {
+  let max = value[0]
+  let pos = 0
+  for (let i = 1; i < value.length; i++) {
+    if (value[i] > max) {
+      max = value[i]
+      pos = i
+    }
+  }
+  return (
+    <div>
+      <h2>Anecdotes with most vote</h2>
+      <p>{show[pos]} </p>
+      <p>has {max} votes</p>
+    </div>
+  )
+};
 
 const App = () => {
   const anecdotes = [
@@ -22,30 +40,34 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast is to go well.'
-  ]
+  ];
 
-  const initialVotesCount = Array(anecdotes.length).fill(0)
-  const [votesCount, setVotes] = useState(initialVotesCount)
-  const [selected, setSelected] = useState(0)
+  const initialVotesCount = Array(anecdotes.length).fill(0);
+  const [votesCount, setVotes] = useState(initialVotesCount);
+  const [selected, setSelected] = useState(0);
 
   const handleRate = () => {
-    const updatedVotesCount = [...votesCount]
-    updatedVotesCount[selected] += 1
-    setVotes(updatedVotesCount)
-  }
+    const updatedVotesCount = [...votesCount];
+    updatedVotesCount[selected] += 1;
+    setVotes(updatedVotesCount);
+  };
 
   const handleClick = () => {
-    setSelected((selected + 1) % anecdotes.length)
-  }
+    setSelected(selected + 1);
+  };
 
   return (
     <div>
+      <h2>Anecdotes of the day</h2>
       <p>{anecdotes[selected]} </p>
       <p>has {votesCount[selected]} votes</p>
       <Button handleClick={handleClick} text='next anecdote' />
       <Button2 handleRate={handleRate} text='vote' />
+      <MaxRate value={votesCount} show={anecdotes}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
