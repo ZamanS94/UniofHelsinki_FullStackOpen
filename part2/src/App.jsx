200d507e2a9eb463285c12a16e85d9704f4205b3
Sabina Zaman <sabina.zaman94@gmail.com>
2024-02-12 14:36:06 +0100

    2.13: The Phonebook step 8

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,70 +1,62 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Person from './component/persons'
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([]);
   const [newPersonName, setNewPersonName] = useState('')
   const [newPersonNumber, setNewPersonNumber] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios.get('http://localhost:3001/persons')
-      .then((response) => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error.message)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons);
       })
   }, [])
 
+  // we use this to read all the data from database
 
-  const addPerson = (event) => {
-    event.preventDefault()
+  const addPerson = event => {
+    event.preventDefault();
     const personObject = {
       name: newPersonName,
       number: newPersonNumber,
-      id: persons.length + 1
     }
   
-    axios
-    .post('http://localhost:3001/persons', personObject)
-    .then(response => {
-      setPersons(persons.concat(personObject))
-      setNewPersonName('')
-      setNewPersonNumber('')
-    })
+    personService
+      .create(personObject)
+      .then(response => {
+        setPersons([...persons, response])
+        setNewPersonName('')
+        setNewPersonNumber('')
+      })
   }
+  
+// using this to create new object and then concate to old list
 
-  const handleNameChange = (event) => {
+  const handleNameChange = event => {
     setNewPersonName(event.target.value)
   }
 
-  const handleNumberChange = (event) => {
+  const handleNumberChange = event => {
     setNewPersonNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      {persons.map((person) => (
+      {persons.map(person => (
         <Person key={person.id} person={person} />
       ))}
       <form onSubmit={addPerson}>
         <div>
           name:
-          <input
-            value={newPersonName}
-            onChange={handleNameChange}
-          />
+          <input value={newPersonName} onChange={handleNameChange} />
         </div>
         <div>
           number:
-          <input
-            value={newPersonNumber}
-            onChange={handleNumberChange}
-          />
+          <input value={newPersonNumber} onChange={handleNumberChange} />
         </div>
         <button type="submit">add</button>
       </form>
